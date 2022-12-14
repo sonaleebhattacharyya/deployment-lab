86b9e2fb490db8e5ce822a79369e4824c98a3fcc
Sonalee Bhattacharyya <sonaleebhattacharyya@gmail.com>
2022-12-14 17:16:50 -0600

    dplab

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.static(`${__dirname}/public`))
+app.get('/',(req,res)=> {
+    res.sendFile(path.join(_dirname,'../public/index.html'))
+})
 app.listen(4000, () => console.log(`server running on 4000`))
